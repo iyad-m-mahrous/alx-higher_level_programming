f51412d8401e5a4ad6b0d43eec0634ede2db814e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-05 03:52:30 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(10):
+    for j in range(i+1, 10):
+        if (i != j) and (i < 8):
+            print('{}{}, '.format(i, j), end='')
+        elif (i == 8):
+            print('{}{}'.format(i, j))
