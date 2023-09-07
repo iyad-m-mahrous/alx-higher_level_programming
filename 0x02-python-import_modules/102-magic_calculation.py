fd360c6530447c1a256b531996e554c195910450
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-07 03:46:25 -0700

    Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    add = getattr(importlib.import_module("magic_calculation_102"), "add")
-    sub = getattr(importlib.import_module("magic_calculation_102"), "sub")
+    add = __import__('magic_calculation_102', globals(), locals(), ['add'], 0).__dict__['add']
+    sub = __import__('magic_calculation_102', globals(), locals(), ['sub'], 0).__dict__['sub']
 
     if a < b:
         c = add(a, b)
         for i in range(4, 7):
             c = add(c, i)
+        return c
     else:
-        c = sub(a, b)
+        return sub(a, b)
 
-    return c
