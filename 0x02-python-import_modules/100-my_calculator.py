b28e1955dfcb4d944ce3043926c7f78d3cb4cc52
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-07 03:17:23 -0700

    removing match since python ver < 3.10

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-def task6():
-    args = sys.argv[1:]
-    length = len(args)
-    result = 0
+    def task6():
+        args = sys.argv[1:]
+        length = len(args)
+        result = 0
 
-    if length != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        sys.exit(1)
+        if length != 3:
+            print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+            sys.exit(1)
 
-    if args[1] not in ['+', '-', '*', '/']:
-        print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
+        if args[1] not in ['+', '-', '*', '/']:
+            print('Unknown operator. Available operators: +, -, * and /')
+            sys.exit(1)
 
-    a = int(args[0])
-    b = int(args[2])
+        a = int(args[0])
+        b = int(args[2])
 
-    if args[1] == '+':
-        result = add(a, b)
-    elif args[1] == '-':
-        result = sub(a, b)
-    elif args[1] == '*':
-        result = mul(a, b)
-    else:
-        result = div(a, b)
+        if args[1] == '+':
+            result = add(a, b)
+        elif args[1] == '-':
+            result = sub(a, b)
+        elif args[1] == '*':
+            result = mul(a, b)
+        else:
+            result = div(a, b)
 
-    print(f'{args[0]} {args[1]} {args[2]} = {result}')
+        print(f'{args[0]} {args[1]} {args[2]} = {result}')
 
+    def main():
+        task6()
 
-def main():
-    task6()
 
 if __name__ == '__main__':
     main()
