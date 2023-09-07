a4590ff3d67bd154dcfdbae609fc3037c57cd35f
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-07 02:05:33 -0700

    tasks 0-5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def task4():
+    names_list = dir(hidden_4)
+    for name in names_list:
+        if name[:2] != "__":
+            print(f'{name}')
+
+
+def main():
+    task4()
+
+
+if __name__ == '__main__':
+    main()
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
+
+def task5():
+    print(f'{a}')
+
+
+def main():
+    task5()
+
+
+if __name__ == '__main__':
+    main()
