def690bdf475c4077deb8954aa6dadd84141f908
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-07 03:03:26 -0700

    removing match since python ver < 3.10

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,22 +16,20 @@ def task6():
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
 
-    match args[1]:
-        case '+':
-            result = add(int(args[0]), int(args[]2))
-        case '-':
-            result = sub(int(args[0]), int(args[]2))
-        case '*':
-            result = mul(int(args[0]), int(args[]2))
-        case '/':
-            result = div(int(args[0]), int(args[]2))
+    if args[1] == '+':
+        result = add(int(args[0]), int(args[2]))
+    elif args[1] == '-':
+        result = sub(int(args[0]), int(args[2]))
+    elif args[1] == '*':
+        result = mul(int(args[0]), int(args[2]))
+    else:
+        result = div(int(args[0]), int(args[2]))
 
-        print(f'{args[0]} {args[1]} {args[2]} = {result}')
+    print(f'{args[0]} {args[1]} {args[2]} = {result}')
 
 
 def main():
     task6()
-
 
 if __name__ == '__main__':
     main()
