9e8b298e7587c2037ab64b3b8abba1355723a84a
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-08 22:44:48 -0700

    adding {:d}

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i[:-1]:
-            print('{} '.format(j), end='')
-        print('{}'.format(i[-1]))
+            print('{:d} '.format(j), end='')
+        print('{:d}'.format(i[-1]))
