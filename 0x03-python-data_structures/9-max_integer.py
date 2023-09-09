38e39c81ae913e1b9dcc5b5782fc857eb794ce19
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-08 22:58:00 -0700

    fixing condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = 0
     if my_list == []:
         return None
-    for i in my_list:
-        if i > max_num:
-            max_num = i
-    return max_num
+    mylist.sort()
+    return mylist[-1]
