1247fdc37b3cdfbaca45ece7f39d9f9fa4a3bf1a
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-08 23:23:01 -0700

    fixing format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        return
     for i in matrix:
         for j in i[:-1]:
             print('{:d} '.format(j), end='')
-        print('{:d}'.format(i[-1]), end='')
-        print()
+        print('{:d}'.format(i[-1]))
