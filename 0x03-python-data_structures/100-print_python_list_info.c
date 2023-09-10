9d2f84787c58a098d6437bdaff9b490313aaac55
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-10 04:12:50 -0700

    Testing

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
+#include <Python.h>
+
+void print_python_list_info(PyObject *p);
+
 /**
  * print_python_list_info -  prints some basic info about Python lists
  * @p: python objects pointer (list)
