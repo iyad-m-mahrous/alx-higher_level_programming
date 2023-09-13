9735967d00e898f3747c3ba8793556fc1f4b8742
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 03:34:15 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+            }
+    sum = 0
+    prev = 0
+    for i in roman_string:
+        for j in roman_values:
+            if i == j:
+                sum = roman_values[j]
+                if prev < sum:
+                    sum -= prev
+                else:
+                    sum += prev
+                prev = sum
+    return sum
