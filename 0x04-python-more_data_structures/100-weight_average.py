eb62d2dc5a4f58f14916ee8a632b26f9c95c204f
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 04:20:53 -0700

    Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    sum_top = 0
+    sum_bottom = 0
+    for i, j in my_list:
+        sum_top += i*j
+        sum_bottom += j
+    return sum_top / sum_bottom
