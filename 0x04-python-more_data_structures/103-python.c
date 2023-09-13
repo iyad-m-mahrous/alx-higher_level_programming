df9caa38cb34b2339e6a3efd29d85ccf4116190e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 14:48:36 -0700

    Task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -17,7 +17,7 @@ void print_python_bytes(PyObject *p)
         printf("[.] bytes object info\n");
         if (strcmp(p->ob_type->tp_name, "bytes") != 0)
         {
-                printf("  [ERROR] Invalid Bytes Object\n");
+                fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
                 return;
         }
 
