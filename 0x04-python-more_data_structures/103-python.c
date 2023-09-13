f470abb36757005f91c2085ab48fbaf9dd96980a
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 10:20:50 -0700

    Task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -18,17 +18,17 @@ void print_python_list(PyObject *p)
 		fprintf(stderr, "  [ERROR] Invalid List Object\n");
 		return;
 	}
-	Py_ssize_t size = PyList_Size(p);
+	
+	Py_ssize_t size = ((PyVarObject *)p)->ob_size; // Get the size of the list
 	Py_ssize_t i;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %ld: %s\n", i
-				, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+		PyObject *item = ((PyListObject *)p)->ob_item[i]; // Get the item at index i
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
 
@@ -36,7 +36,7 @@ void print_python_list(PyObject *p)
  * print_python_bytes - prints python bytes
  * @p: Python object
  *
- * Retuen: None
+ * Return: None
  */
 void print_python_bytes(PyObject *p)
 {
@@ -46,18 +46,20 @@ void print_python_bytes(PyObject *p)
 		fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	Py_ssize_t size = PyBytes_Size(p);
+
+	Py_ssize_t size = ((PyVarObject *)p)->ob_size; // Get the size of the bytes object
 	Py_ssize_t i;
 
 	printf("[.] bytes object info\n");
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", PyBytes_AsString(p));
+	printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+
 	if (size > 10)
 	{
 		printf("  first 10 bytes: ");
 		for (i = 0; i < 10; i++)
 		{
-			printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+			printf("%02x ", (unsigned char)((PyBytesObject *)p)->ob_sval[i]);
 		}
 		printf("\n");
 	}
@@ -66,7 +68,7 @@ void print_python_bytes(PyObject *p)
 		printf("  first %ld bytes: ", size);
 		for (i = 0; i < size; i++)
 		{
-			printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+			printf("%02x ", (unsigned char)((PyBytesObject *)p)->ob_sval[i]);
 		}
 		printf("\n");
 	}
