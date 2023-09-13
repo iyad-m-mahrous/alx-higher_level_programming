a3ad542ee35952c7c1dd1ee3d36fd1bac228cc37
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 11:13:14 -0700

    Task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -49,8 +49,10 @@ void print_python_bytes(PyObject *p)
 	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
 	printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
 
-	printf("  first %ld bytes: ", ((PyVarObject *)p)->ob_size);
-	for (Py_ssize_t i = 0; i <= ((PyVarObject *)p)->ob_size; i++)
+	printf("  first %ld bytes: ",
+		       ((PyVarObject *)p)->ob_size + 1 >
+		      10 ? 10 : ((PyVarObject *)p)->ob_size + 1);
+	for (Py_ssize_t i = 0; i < 10 && i <= ((PyVarObject *)p)->ob_size; i++)
 	{
 		printf("%02x", (unsigned char)(((PyBytesObject *)p)->ob_sval[i]));
 		if (i < ((PyVarObject *)p)->ob_size)
