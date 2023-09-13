52a7b302a65691cbdfd46c3c5cd21e1c9df07eef
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-13 08:41:02 -0700

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = list[a_dictionary.keys()]
+    [a_dictionary.pop(i) for i in keys if a_dictionary[i] == value]
+    return a_dictionary
