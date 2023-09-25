b11fccb822de5b7f90187231bdd3e170762d88a6
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-25 02:30:53 -0700

    Fixing Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end='')
-    except IndexError:
-        i -= 1
-    finally:
-        print()
-        return (i+1)
+    '''prints x elements of a list.
+    '''
+    count = 0
+    for i in range(x):
+        try:
+            print(mylist[i], end='')
+            count += 1
+        except IndexError:
+            break
+    print()
+    return (count)
