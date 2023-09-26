cc91292d154727997fc53f3a8a967f5334a20aff
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-26 02:18:58 -0700

    fixing Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,19 @@
 
 
 class Square:
-    """Class Square that defines a square."""
+    """
+    A class representing a square.
+
+    This class is a basic representation of a square shape.
+    It doesn't have any methods or attributes defined at the moment.
+
+    Attributes:
+        No attributes are defined in this class.
+
+    Methods:
+        No methods are defined in this class.
+
+    Usage:
+        square = Square()
+    """
     pass
