f84758fb3013a0a34b21e00a0d56752e4d64020d
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-26 05:04:47 -0700

    Task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""ByteCode -> Python #5"""
+
+
+class MagicClass:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return math.pi * self._MagicClass__radius ** 2
+
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
