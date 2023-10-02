0a86ac82ba0f2b195fef4156bc9ea70ae63ab174
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-02 14:49:37 -0700

    Task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,8 +12,11 @@ def text_indentation(text):
 
     if (not isinstance(text, str)):
         raise TypeError("text must be a string")
-    new_text = text.strip()
-    new_text = "\n\n".join([i.strip() for i in new_text.split(".")])
-    new_text = "\n\n".join([i.strip() for i in new_text.split(":")])
-    new_text = "\n\n".join([i.strip() for i in new_text.split("?")])
+    new_text = "\n".join([y.strip() for y in text.splitlines()])
+    new_text = ".".join([y.strip(" \t") for y in new_text.split(".")])
+    new_text = ":".join([y.strip(" \t") for y in new_text.split(":")])
+    new_text = "?".join([y.strip(" \t") for y in new_text.split("?")])
+    new_text = new_text.replace(".", ".\n\n")
+    new_text = new_text.replace(":", ":\n\n")
+    new_text = new_text.replace("?", "?\n\n")
     print(new_text, end="")
