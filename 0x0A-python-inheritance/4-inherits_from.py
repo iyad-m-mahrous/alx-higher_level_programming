6857e5d73ee098175ccdd175dacf59500440fb72
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-09 03:16:54 -0700

    Initialization

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a function that returns True if the object is an instancei
+of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False."""
 
 
 def inherits_from(obj, a_class):
