c659b92a23f7bdcb95f6ff57bb4ed0cd2df81203
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-09 04:00:03 -0700

    Test

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" a class MyInt that inherits from int"""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """class MyInt(int):"""
+    """Invert int operators == and !=."""
 
     def __ne__(self, other):
         return (super.__eq__(other))
