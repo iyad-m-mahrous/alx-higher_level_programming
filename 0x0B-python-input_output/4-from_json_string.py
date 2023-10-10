0eb10c3196db228257172e2cac4837f700f29389
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 02:57:51 -0700

    Task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""a function that returns an object (Python data structure)
+represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """def from_json_string(my_str):"""
+
+    return (json.load(my_str))
