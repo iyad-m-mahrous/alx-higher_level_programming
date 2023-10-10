04619196ea8bdaab97466c4a6c1205ec0f23f124
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 02:41:02 -0700

    Fixing Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     """def write_file(filename="", text="")"""
 
     with open(filename, 'w', encoding='utf-8') as f:
-        return(f.write(text))
+        return (f.write(text))
