ff66837a0640e3f397171539b1d256d93d8cef35
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 03:25:51 -0700

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ def save_to_json_file(my_obj, filename):
     """def save_to_json_file(my_obj, filename)"""
 
     with open(filename, 'w', encoding='UTF-8') as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
