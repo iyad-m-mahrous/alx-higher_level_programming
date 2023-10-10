16d9255000217000e13b4eff15d9ee54da1db3e6
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 13:00:27 -0700

    Task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,16 +6,13 @@ after each line containing a specific string"""
 def append_after(filename="", search_string="", new_string=""):
     """def append_after(filename="", search_string="", new_string="")"""
 
-    try:
-        with open(filename, "r+", encoding='UTF-8') as f:
-            lines = f.readlines()
-            f.seek(0)
-            for line in lines:
-                if "Holberton " in line:
-                    line = line.replace("Holberton ", "")
+    with open(filename, "r+", encoding='UTF-8') as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if "Holberton " in line:
+                line = line.replace("Holberton ", "")
                 f.write(line)
                 if search_string in line:
                     f.write(new_string)
-            f.truncate()
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
+        f.truncate()
