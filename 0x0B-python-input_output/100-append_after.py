ca033a25811bb333997461fe48bf9a9fbeae2027
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 12:36:25 -0700

    Task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" a function that inserts a line of text to a file,
+after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """def append_after(filename="", search_string="", new_string="")"""
+
+    try:
+        with open(filename, "r+", encoding='UTF-8') as f:
+            lines = f.readlines()
+            f.seek(0)
+            for line in lines:
+                f.write(line)
+                if search_string in line:
+                    f.write(new_string + '\n')
+            f.truncate()
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
