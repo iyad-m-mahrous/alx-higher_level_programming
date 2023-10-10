c22577f69d4ce98c63a8f29aaf154db25756adbf
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 05:59:35 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,10 @@ import sys
 def add_item():
 
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = (
+            __import__('6-load_from_json_file')
+            .load_from_json_file
+    )
 
     try:
         loaded = load_from_json_file("add_item.json")
@@ -17,6 +20,7 @@ def add_item():
     args = sys.argv[1:]
     loaded.extend(args)
     save_to_json_file(loaded, "add_item.json")
+
 
 if __name__ == "__main__":
     add_item()
