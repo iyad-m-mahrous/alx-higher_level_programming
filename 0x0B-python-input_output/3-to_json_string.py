f83569de0489355b7a976e1f4752a692f9f4558e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-10 02:49:04 -0700

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""a function that returns the JSON representation
+of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """def to_json_string(my_obj):"""
+
+    return (json.dump(my_obj))
