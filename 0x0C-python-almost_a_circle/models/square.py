aa440539bab3fa3041b6e03f6b3977f2c0574e7f
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-16 08:46:10 -0700

    mod square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """Square Class"""
-from .rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """class Square(Rectangle):"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """def __str__(self):"""
+
         return (f'[Square] ({self.id}) {self.x}/{self.y} - '
                 f'{self.width}')
 
     @property
     def size(self):
+        """def size(self):"""
+
         return self.width
 
     @size.setter
@@ -23,6 +36,17 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+
         if args and len(args) != 0:
             no_args = len(args)
             if (no_args > 0):
@@ -42,6 +66,8 @@ class Square(Rectangle):
             self.y = kwargs.get('y', self.y)
 
     def to_dictionary(self):
+        """def to_dictionary(self):"""
+
         return {
             'id': self.id,
             'size': self.size,
