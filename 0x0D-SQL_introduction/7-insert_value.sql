a096f2fce4fbd1651755059ffb7cdfe68230e649
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-14 02:35:47 -0800

    Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- 7. First add
---Write a script that inserts a new row in the table 
+-- Write a script that inserts a new row in the table 
 --  first_table (database hbtn_0c_0) in your MySQL server.
 -- New row:
 -- id = 89
