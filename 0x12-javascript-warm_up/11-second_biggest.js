a622979c3c76446b3980b2dd9616b4d98ce2435f
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-11 06:19:33 -0800

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ let max = -Infinity;
 let max2 = 0;
 
 process.argv.forEach((num) => {
-  if (!isNan(num) && Number(num) > max) {
+  if (Number(num) > max) {
     max !== -Infinity ? max2 = max : max2 = 0;
     max = num;
   }
