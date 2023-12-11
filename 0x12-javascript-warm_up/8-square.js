188689cb52e8b81484fa7931a99a1b2c38583e37
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-11 04:42:52 -0800

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < process.argv[2]; x++) {
+    let line = '';
+    for (let y = 0; y < process.argv[2]; y++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
