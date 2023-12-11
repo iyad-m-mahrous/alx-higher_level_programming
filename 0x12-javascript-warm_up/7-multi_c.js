8901fcdeb177c2f69ce13a1ad7a62d2213a3673d
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-11 04:14:35 -0800

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let count = 0; count < Number(process.argv[2]); count++) {
+    console.log('C is fun');
+  }
+}
