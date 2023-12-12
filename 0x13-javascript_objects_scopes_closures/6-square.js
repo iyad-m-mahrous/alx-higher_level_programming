c1809cdc2cd13e28a0ae51c271c71f49c421e54b
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-12 07:37:37 -0800

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let x = 0; x < this.height; x++) console.log('c'.repeat(this.width));
+      for (let x = 0; x < this.height; x++) console.log(c.repeat(this.width));
     }
   }
 };
