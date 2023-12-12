621a3db7886773a5330979e8b5ed6236b8692956
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-12 08:12:04 -0800

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   return list.reduce((count, current) => {
-    return current === searchElement ? count++ : count;
+    return current === searchElement ? ++count : count;
   }, 0);
 };
