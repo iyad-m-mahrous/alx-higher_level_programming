bdcb69185837d71a542437b069f48beba0665951
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 03:12:12 -0800

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 1. Filter states """
+""" 0. Get all states """
 import sys
 import MySQLdb
 
@@ -13,8 +13,7 @@ if __name__ == '__main__':
         charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE states.name LIKE 'N%'"
-                "ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
