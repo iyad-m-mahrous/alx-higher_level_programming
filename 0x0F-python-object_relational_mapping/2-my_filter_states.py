90ba67bcf6bd494fe94438d7ca4fd61f21b11eed
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 01:43:51 -0800

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" 2. Filter states by user input """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM `states` WHERE states.name = '{sys.argv[4]}'"
+                f"ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
