92fa0c47be4241fa62dc7605ef60ec089ba6927d
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 05:18:07 -0800

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" 4. Cities by states """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities as c\
+                INNER JOIN states as s ON s.id = c.state_id\
+                ORDER BY c.id")
+    query_rows = cur.fetchall()
+    [print(row) for row in query_rows]
+    cur.close()
+    db.close()
