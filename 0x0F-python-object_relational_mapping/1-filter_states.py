9a12c2387eb6ff0486e5f826efd6709777271ace
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 03:50:16 -0800

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,12 @@ if __name__ == '__main__':
         charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
-    query_rows = cur.fetchall()
-    [print(row) for row in query_rows if row[1][0] == 'N']
+    try:
+        cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
+        query_rows = cur.fetchall()
+    except Exception as e:
+        pass
+    else:
+        [print(row) for row in query_rows]
     cur.close()
     db.close()
