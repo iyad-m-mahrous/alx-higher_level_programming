a44d5f05bc43e199f98953cc74a780578a42ea40
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 03:31:14 -0800

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM `states` WHERE states.name = '{}'\
                 ORDER BY states.id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    [print(row) for row in query_rows if row]
     cur.close()
     db.close()
