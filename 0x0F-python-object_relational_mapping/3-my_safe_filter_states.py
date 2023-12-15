014c44ce77a367bfd3994974c4434c36c5cc22f0
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 04:25:14 -0800

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" 3. SQL Injection... """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE BINARY name = %s\
+                ORDER BY id", (sys.argv[4],))
+    query_rows = cur.fetchall()
+    [print(row) for row in query_rows]
+    cur.close()
+    db.close()
