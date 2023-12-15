022f0c027550bbb0daf31a99b2df4fcd4e61168a
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 12:03:26 -0800

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""13. Delete states
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State)\
+        .filter(func.binary(State.name).like('%a%')).all()
+    [delete(row) for row in rows]
+    session.commit()
