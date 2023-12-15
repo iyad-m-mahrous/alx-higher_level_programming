96383bd5ba05b422e7b665144a66c5cc974ed701
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-15 00:57:51 -0800

    Tasks 0-1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
             passwd=sys.argv[2], db=sys.argv[3], charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%'\
-                ORDER BY states.id ASC")
+                ORDER BY `id` ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
